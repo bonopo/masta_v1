baf22689fb4f60976be0b3e519a88b5c56dc0ee7
bonopo <raphmenke@gmail.com>
2018-10-12 10:47:52 +0200

    ssi caclulated according to gaussian distribution uncommented spi/SPEI calculation

--- R_v1.R
+++ R_v1.R
@@ -67,184 +67,184 @@ temp_long %<>% filter(date>= "1970-01-01" & date <= "2009-12-31")
 
 
 
-# Cluster calculation -----------------------------------------------------
-#seasonality ratio (SR)
-
-# creating two time series one winter one summer
-#calculating q95 for both parts
-q_sr_w <- q_long %>% 
-  mutate(month = month(date)) %>% 
-  filter(month > 11 | month <4) %>%  #definitin of winter from Laaha et al 2006
-  group_by(gauge) %>% 
-  mutate(qt = quantile(q, 0.05)) %>% 
-  summarise(q95_w = mean(qt))
-
-q_sr_s <- q_long %>% 
-  mutate(month = month(date)) %>% 
-  filter(month < 12 | month > 3) %>% 
-  group_by(gauge) %>% 
-  mutate(qt = quantile(q, 0.05)) %>% 
-  summarise(q95_s = mean(qt))
-
-q_sr <- merge(q_sr_s, q_sr_w, by="gauge")
-q_sr$sr <- q_sr$q95_s/q_sr$q95_w # SR is caclulated via q95_summer/q95_winter from Laaha et al 2006
-
-q_sr$sr_value[which(q_sr$sr < 1)] <- 0 #summer
-q_sr$sr_value[which(q_sr$sr > 1)] <- 1 #winter NAs are produced in a 9 time series that are very altered or have no clear seasonality 
-
-gauges$sr <- as.numeric(q_sr$sr_value)
-# spplot(gauges, "sr")
-  
-# ind <- which(is.na(q_sr$sr_value))
-# q_long %>%
-#   filter(gauge %in% ind) %>%
-#   filter(year(date) < 1975) %>%
-#   ggplot() +
-#   geom_smooth(aes(x=date, y=q, group= as.factor(gauge), color= as.factor(gauge)))+
-#   scale_y_log10()
-
-
-# SPI calculation ---------------------------------------------------------
-
-
-# # rough estimate of gamma distribution parameters
-# para_ini <- precip_long %>% 
+# # Cluster calculation -----------------------------------------------------
+# #seasonality ratio (SR)
+# 
+# # creating two time series one winter one summer
+# #calculating q95 for both parts
+# q_sr_w <- q_long %>% 
+#   mutate(month = month(date)) %>% 
+#   filter(month > 11 | month <4) %>%  #definitin of winter from Laaha et al 2006
 #   group_by(gauge) %>% 
-#   summarise(shape_p = dist.start(sum_mm, "gamma")[[1]], rate_p = dist.start(sum_mm, "gamma")[[2]] )
-
-#aggregating into montly sums
-
-precip_monthly <- precip_long %>% 
-  mutate(yr_mt =  ymd(paste0(year(date),"-", month(date),"-","15"))) %>% 
-  group_by(gauge,yr_mt) %>% 
-  summarise(month_sum = sum(sum_mm)) %>% 
-  ungroup()
-
-#1. Step: fitting of distribution to aggregated data with SCI package 
-#2. Step: transform to standart normal distribuion with mean: 0 and sd: 1
-#with SCI package
-spi <- as.list(NA)
-for(i in unique(precip_monthly$gauge)){
-data <- precip_monthly$month_sum[precip_monthly$gauge==i]
-params_dist <- fitSCI(data, first.mon = 1, distr = "gamma", time.scale = 6, p0 =T)
-#inital values for parameters calculated with L-moments and then optimization with maximum likelihood
-spi_temp <- transformSCI(data, first.mon = 1, obj = params_dist)
-spi[[i]] <- spi_temp
-}
-
-
-# plot(spi[[1]], type="l")
-
-
-# SPEI Calculation --------------------------------------------------------
-#with SPEI package and SCI
-
-# PET calculation with thornwaite
-mt_mn_temp <- temp_long %>% 
-  mutate(yr_mt =  ymd(paste0(year(date),"-", month(date),"-","15"))) %>% 
-  group_by(gauge, yr_mt) %>% 
-  summarise(temp_m = mean(temp)) %>% 
-  ungroup()
-
-
-#Gauss Krueger converted to WGS84
-xy_gk <- cbind.data.frame("x_gk" = 4475806, "y_gk"= gauges$Hochwrt) #only latitude is needed, therefore random x value
-coordinates(xy_gk) <-  c("x_gk", "y_gk")
-proj4string(xy_gk) <- CRS("+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs")
-xy_wgs84 <- spTransform(xy_gk, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-
-
-latitude <- coordinates(xy_wgs84) %>% 
-  as.data.frame() %>% 
-  dplyr::select(y_gk) %>% 
-  cbind("gauge" = unique(mt_mn_temp$gauge)) %>% 
-  as.tbl()
-
-#calculating Potential Evapotranspiration with thornthwaite
-pet_th <- list(NA)
-for(i in unique(mt_mn_temp$gauge)){
-  data <- mt_mn_temp$temp_m[mt_mn_temp$gauge==i]
-  res_ts <- ts(data, frequency=12, start=c(1970,1))
-  pet_th[[i]] <- thornthwaite(data,latitude$y_gk[latitude$gauge==i] )
-}
-
-#calculating SPEI: P - PET
-pet_th_vec <- pet_th[unique(mt_mn_temp$gauge)] %>% 
-  unlist() %>% 
-  as.numeric()
-spei_data <-precip_monthly %>% 
-    mutate(pet_th = pet_th_vec) %>% 
-    mutate(p_pet = month_sum - pet_th)
-
-sci_calc <- function(datax = spei_data$p_pet, gaugex=spei_data$gauge, distx="gev", agg_n=6, p0x=F){
- output <- as.list(NA)
-for(i in unique(gaugex)){
-data <- datax[gaugex==i]
-params_dist <- fitSCI(data, first.mon = 1, distr = distx, time.scale = agg_n, p0 =p0x)
-#inital values for parameters calculated with L-moments and then optimization with maximum likelihood
-#if there are a lot of zeros in the time series it is recommended to use p0=TRUE
-spi_temp <- transformSCI(data, first.mon = 1, obj = params_dist)
-output[[i]] <- spi_temp
-}  
- return(output)
-  }
-
-# with Generalized Logistic Distribution                  
-spei_gl <- sci_calc(distx = "genlog")
-
-#with Generalized extreme value distribution
-spei_gev <- sci_calc()
-
-#with gamma distribution
-spei_gam <- sci_calc(distx="gamma") #bad result three parameter distribution used for arid regions because it can handle negative values better vicente-serrano et al 2010
-
-#with log-logistic distribution (different function from SPEI package)
-
-spei_llogis <- as.list(NA)
-for(i in unique(spei_data$gauge)){
-data <- spei_data$p_pet[spei_data$gauge==i]
-spei_llogis[[i]] <- spei(data, scale=6)
-}   
-
-#converting SPEI objects into vectors
-spei_vec <- function(data, spei=FALSE, gaugex=mt_mn_temp$gauge){
-  output <-  vector()
-  for(i in unique(gaugex)){
-  temp <- unclass(data[[i]]) 
-  if(spei == FALSE){
-    output <- c(output, as.numeric(temp))}else{
-    output <- c(output, as.numeric(temp$fitted))}
-  }
-  return(output)
-  }
-
-spei_ll_v <- spei_vec(spei_llogis,spei= TRUE)
-spei_gv_v <- spei_vec(spei_gev)
-spei_gl_v <- spei_vec(spei_gl)
-
-#which distribution fits best
-#merging data
-
-spei <- cbind(spei_ll_v, spei_gv_v, spei_gl_v) %>% 
-  as.data.frame() %>% 
-  as.tbl() %>% 
-  mutate(date= mt_mn_temp$yr_mt) %>% 
-  mutate(gauge= mt_mn_temp$gauge )
-
-spei_long <- gather(spei, distr, value, -date, -gauge)
-  spei_long %>% 
-    filter(date < 1980 & gauge == 1) %>% 
-ggplot()+
-  geom_line(aes(x= date, y=value,  color=distr), stat="identity")
-
-#L-moments diagrams
-  
-#Kolmogorow-Smirnow-Test
-  ks.test(x=spei_data$p_pet, y=spei$spei_ll_v, alternative = "t" )
-  ks.test(x=spei_data$p_pet, y=spei$spei_gv_v, alternative = "t" )
-  ks.test(x=spei_data$p_pet, y=spei$spei_gl_v, alternative = "t" )
-  # similar d values the lower the better
-  
+#   mutate(qt = quantile(q, 0.05)) %>% 
+#   summarise(q95_w = mean(qt))
+# 
+# q_sr_s <- q_long %>% 
+#   mutate(month = month(date)) %>% 
+#   filter(month < 12 | month > 3) %>% 
+#   group_by(gauge) %>% 
+#   mutate(qt = quantile(q, 0.05)) %>% 
+#   summarise(q95_s = mean(qt))
+# 
+# q_sr <- merge(q_sr_s, q_sr_w, by="gauge")
+# q_sr$sr <- q_sr$q95_s/q_sr$q95_w # SR is caclulated via q95_summer/q95_winter from Laaha et al 2006
+# 
+# q_sr$sr_value[which(q_sr$sr < 1)] <- 0 #summer
+# q_sr$sr_value[which(q_sr$sr > 1)] <- 1 #winter NAs are produced in a 9 time series that are very altered or have no clear seasonality 
+# 
+# gauges$sr <- as.numeric(q_sr$sr_value)
+# # spplot(gauges, "sr")
+#   
+# # ind <- which(is.na(q_sr$sr_value))
+# # q_long %>%
+# #   filter(gauge %in% ind) %>%
+# #   filter(year(date) < 1975) %>%
+# #   ggplot() +
+# #   geom_smooth(aes(x=date, y=q, group= as.factor(gauge), color= as.factor(gauge)))+
+# #   scale_y_log10()
+# 
+# 
+# # SPI calculation ---------------------------------------------------------
+# 
+# 
+# # # rough estimate of gamma distribution parameters
+# # para_ini <- precip_long %>% 
+# #   group_by(gauge) %>% 
+# #   summarise(shape_p = dist.start(sum_mm, "gamma")[[1]], rate_p = dist.start(sum_mm, "gamma")[[2]] )
+# 
+# #aggregating into montly sums
+# 
+# precip_monthly <- precip_long %>% 
+#   mutate(yr_mt =  ymd(paste0(year(date),"-", month(date),"-","15"))) %>% 
+#   group_by(gauge,yr_mt) %>% 
+#   summarise(month_sum = sum(sum_mm)) %>% 
+#   ungroup()
+# 
+# #1. Step: fitting of distribution to aggregated data with SCI package 
+# #2. Step: transform to standart normal distribuion with mean: 0 and sd: 1
+# #with SCI package
+# spi <- as.list(NA)
+# for(i in unique(precip_monthly$gauge)){
+# data <- precip_monthly$month_sum[precip_monthly$gauge==i]
+# params_dist <- fitSCI(data, first.mon = 1, distr = "gamma", time.scale = 6, p0 =T)
+# #inital values for parameters calculated with L-moments and then optimization with maximum likelihood
+# spi_temp <- transformSCI(data, first.mon = 1, obj = params_dist)
+# spi[[i]] <- spi_temp
+# }
+# 
+# 
+# # plot(spi[[1]], type="l")
+# 
+# 
+# # SPEI Calculation --------------------------------------------------------
+# #with SPEI package and SCI
+# 
+# # PET calculation with thornwaite
+# mt_mn_temp <- temp_long %>% 
+#   mutate(yr_mt =  ymd(paste0(year(date),"-", month(date),"-","15"))) %>% 
+#   group_by(gauge, yr_mt) %>% 
+#   summarise(temp_m = mean(temp)) %>% 
+#   ungroup()
+# 
+# 
+# #Gauss Krueger converted to WGS84
+# xy_gk <- cbind.data.frame("x_gk" = 4475806, "y_gk"= gauges$Hochwrt) #only latitude is needed, therefore random x value
+# coordinates(xy_gk) <-  c("x_gk", "y_gk")
+# proj4string(xy_gk) <- CRS("+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs")
+# xy_wgs84 <- spTransform(xy_gk, CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+# 
+# 
+# latitude <- coordinates(xy_wgs84) %>% 
+#   as.data.frame() %>% 
+#   dplyr::select(y_gk) %>% 
+#   cbind("gauge" = unique(mt_mn_temp$gauge)) %>% 
+#   as.tbl()
+# 
+# #calculating Potential Evapotranspiration with thornthwaite
+# pet_th <- list(NA)
+# for(i in unique(mt_mn_temp$gauge)){
+#   data <- mt_mn_temp$temp_m[mt_mn_temp$gauge==i]
+#   res_ts <- ts(data, frequency=12, start=c(1970,1))
+#   pet_th[[i]] <- thornthwaite(data,latitude$y_gk[latitude$gauge==i] )
+# }
+# 
+# #calculating SPEI: P - PET
+# pet_th_vec <- pet_th[unique(mt_mn_temp$gauge)] %>% 
+#   unlist() %>% 
+#   as.numeric()
+# spei_data <-precip_monthly %>% 
+#     mutate(pet_th = pet_th_vec) %>% 
+#     mutate(p_pet = month_sum - pet_th)
+# 
+# sci_calc <- function(datax = spei_data$p_pet, gaugex=spei_data$gauge, distx="gev", agg_n=6, p0x=F){
+#  output <- as.list(NA)
+# for(i in unique(gaugex)){
+# data <- datax[gaugex==i]
+# params_dist <- fitSCI(data, first.mon = 1, distr = distx, time.scale = agg_n, p0 =p0x)
+# #inital values for parameters calculated with L-moments and then optimization with maximum likelihood
+# #if there are a lot of zeros in the time series it is recommended to use p0=TRUE
+# spi_temp <- transformSCI(data, first.mon = 1, obj = params_dist)
+# output[[i]] <- spi_temp
+# }  
+#  return(output)
+#   }
+# 
+# # with Generalized Logistic Distribution                  
+# spei_gl <- sci_calc(distx = "genlog")
+# 
+# #with Generalized extreme value distribution
+# spei_gev <- sci_calc()
+# 
+# #with gamma distribution
+# spei_gam <- sci_calc(distx="gamma") #bad result three parameter distribution used for arid regions because it can handle negative values better vicente-serrano et al 2010
+# 
+# #with log-logistic distribution (different function from SPEI package)
+# 
+# spei_llogis <- as.list(NA)
+# for(i in unique(spei_data$gauge)){
+# data <- spei_data$p_pet[spei_data$gauge==i]
+# spei_llogis[[i]] <- spei(data, scale=6)
+# }   
+# 
+# #converting SPEI objects into vectors
+# spei_vec <- function(data, spei=FALSE, gaugex=mt_mn_temp$gauge){
+#   output <-  vector()
+#   for(i in unique(gaugex)){
+#   temp <- unclass(data[[i]]) 
+#   if(spei == FALSE){
+#     output <- c(output, as.numeric(temp))}else{
+#     output <- c(output, as.numeric(temp$fitted))}
+#   }
+#   return(output)
+#   }
+# 
+# spei_ll_v <- spei_vec(spei_llogis,spei= TRUE)
+# spei_gv_v <- spei_vec(spei_gev)
+# spei_gl_v <- spei_vec(spei_gl)
+# 
+# #which distribution fits best
+# #merging data
+# 
+# spei <- cbind(spei_ll_v, spei_gv_v, spei_gl_v) %>% 
+#   as.data.frame() %>% 
+#   as.tbl() %>% 
+#   mutate(date= mt_mn_temp$yr_mt) %>% 
+#   mutate(gauge= mt_mn_temp$gauge )
+# 
+# spei_long <- gather(spei, distr, value, -date, -gauge)
+#   spei_long %>% 
+#     filter(date < 1980 & gauge == 1) %>% 
+# ggplot()+
+#   geom_line(aes(x= date, y=value,  color=distr), stat="identity")
+# 
+# #L-moments diagrams
+#   
+# #Kolmogorow-Smirnow-Test
+#   ks.test(x=spei_data$p_pet, y=spei$spei_ll_v, alternative = "t" )
+#   ks.test(x=spei_data$p_pet, y=spei$spei_gv_v, alternative = "t" )
+#   ks.test(x=spei_data$p_pet, y=spei$spei_gl_v, alternative = "t" )
+#   # similar d values the lower the better
+#   
 # SSI calculation ---------------------------------------------------------
 mt_mn_q <- q_long %>% 
     mutate(yr_mt =  ymd(paste0(year(date),"-", month(date),"-","15"))) %>% 
@@ -287,6 +287,36 @@ for (i in 1:12){
 }
 
 bbb <- jan$V1
+
+# using non parametric solution for SSI -----------------------------------
+
+#fitting to cumulative normal distribution with mean 0 and sd = 1
+
+  gauges_ssi <- data.frame()
+  for (i in 1:12){
+    data <- get(str_to_lower(month.abb[i]))
+    for (g in 1:ncol(data)){
+      gauge <- data[,g]
+      stan <- (gauge-mean(gauge))/sd(gauge)
+      gauges_ssi[(1:length(gauge)),g] <- pnorm(stan)
+      }
+    assign(paste0(str_to_lower(month.abb[i]),"ssi"), gauges_ssi)
+  }
+
+
+j1<- jan$V1
+j1_s<- sort((j1-mean(j1))/sd(j1))
+plot(x=j1_s, y= pnorm(j1_s))
+hist(j1_s)
+pnorm(j1_s)
+n <- length(j1)
+femp <- 1:n/(n+1)  #if not n+1 than that would mean that the probability to measure an event higher than the highest is 0
+pnorm(-1) #probability
+dnorm(-1) #density
+qnorm(.5) #quantiles
+
+# using parametric method for SSI -----------------------------------------
+
 
 #parameter estimation loglikelihood
 #one sided ks test
@@ -362,21 +392,5 @@ output[[i]] <- spi_temp
 }  
  return(output)
   }
-
-#fitting to cumulative normal distribution with mean 0 and sd = 1
-pnorm
-
-j1<- jan$V1
-
-j1_sort <- sort(j1, decreasing = FALSE)
-  n <- length(j1)
-  femp <- 1:n/(n+1)
-  cum_n <- data.frame()
-
-
-  int <- which(near(femp, .5))
-
-  plot(x= Femp,y= j1_sort )
-  cum_n$ssi[int:length(cum_n$ssi)] <- 1/n
 
 
