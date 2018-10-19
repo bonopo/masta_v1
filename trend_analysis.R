e2e75afea0e432a0a835cbd55567a9dd26aeebe5
bonopo <raphmenke@gmail.com>
2018-10-19 15:11:17 +0200

    mann kendall

---
+++ trend_analysis.R
@@ -0,0 +1,12 @@
+
+# Trend analysis ----------------------------------------------------------
+
+source("./R/masta_v1/functions.R")
+source("./R/masta_v1/data_handling.R")
+
+# mann- kendall test ------------------------------------------------------
+
+
+ken_spei <- ken_trend(sci_name = "spei_v2")
+ken_spi <- ken_trend(sci_name="spi_v2")
+ken_ssi <- ken_trend(sci_name= "ssi")
