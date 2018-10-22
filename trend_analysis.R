f220620515a2abde6be6558b15908be5db2901cc
bonopo <raphmenke@gmail.com>
2018-10-22 16:55:14 +0200

    to do: quantil trend

--- trend_analysis.R
+++ trend_analysis.R
@@ -10,3 +10,5 @@ source("./R/masta_v1/data_handling.R")
 ken_spei <- ken_trend(sci_name = "spei_v2")
 ken_spi <- ken_trend(sci_name="spi_v2")
 ken_ssi <- ken_trend(sci_name= "ssi")
+
+# quantil trend ####
