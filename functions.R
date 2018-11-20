049b9821522693152ff9ca83d3f8899ba1398fd3
bonopo <raphmenke@gmail.com>
2018-11-20 15:47:43 +0100

    now working again

--- functions.R
+++ functions.R
@@ -296,9 +296,7 @@ dev.off()
 
 # trend calculaton --------------------------------------------------------
 # ts_data=yearly_mean_q[,1:6]
-# 
- bbsmK_mod(c(jan_mean_df[,1]))
-mmkh_jan_mean_df[1,]
+
 
 mk_tests_par = function(raw_data =c("yearly_mean_q", "yearly_min_q","summer_ave_q","summer_min_q","summer_q_q10")){
 
@@ -416,89 +414,91 @@ bbsmK_mod = function (x, ci = 0.95, nsim = 2000, eta = 1, bl.len = NULL)
     colnames(res) = c(names(MK.orig), "bb_tau","bb_se","lb", "ub")
     return(res)
 }
- # mmkh = function (x, ci = 0.95)
-   # {
-    x = x
-    z = NULL
-    z0 = NULL
-    pval = NULL
-    pval0 = NULL
-    S = 0
-    Tau = NULL
-    essf = NULL
-    ci = ci
-    if (is.vector(x) == FALSE) {
-        stop("Input data must be a vector")
-    }
-    if (any(is.finite(x) == FALSE)) {
-        x <- x[-c(which(is.finite(x) == FALSE))]
-        warning("The input vector contains non-finite numbers. An attempt was made to remove them")
-    }
-    n <- length(x)
-    V <- rep(NA, n * (n - 1)/2)
-    k = 0
-    for (i in 1:(n - 1)) {
-        for (j in (i + 1):n) {
-            k = k + 1
-            V[k] = (x[j] - x[i])/(j - i)
-        }
-    }
-    slp <- median(V, na.rm = TRUE)
-    t = 1:length(x)
-    xn <- (x[1:n]) - ((slp) * (t))
-    for (i in 1:(n - 1)) {
-        for (j in (i + 1):n) {
-            S = S + sign(x[j] - x[i])
-        }
-    }
-    ro <- acf(rank(xn), lag.max = (n - 1), plot = FALSE)$acf[-1]
-    sig <- qnorm((1 + ci)/2)/sqrt(n)
-    rof <- rep(NA, length(ro))
-    for (i in 1:(length(ro))) {
-        if (ro[i] > sig || ro[i] < -sig) {
-            rof[i] <- ro[i]
-        }
-        else {
-            rof[i] = 0
-        }
-    }
-    cte <- 2/(n * (n - 1) * (n - 2))
-    ess = 0
-    for (i in 1:(n - 1)) {
-        ess = ess + (n - i) * (n - i - 1) * (n - i - 2) * rof[i]
-    }
-    essf = 1 + ess * cte
-    var.S = n * (n - 1) * (2 * n + 5) * (1/18)
-    if (length(unique(x)) < n) {
-        aux <- unique(x)
-        for (i in 1:length(aux)) {
-            tie <- length(which(x == aux[i]))
-            if (tie > 1) {
-                var.S = var.S - tie * (tie - 1) * (2 * tie + 
-                  5) * (1/18)
-            }
-        }
-    }
-    VS = var.S * essf
-    if (S == 0) {
-        z = 0
-        z0 = 0
-    }
-    if (S > 0) {
-        z = (S - 1)/sqrt(VS)
-        z0 = (S - 1)/sqrt(var.S)
-    }
-    else {
-        z = (S + 1)/sqrt(VS)
-        z0 = (S + 1)/sqrt(var.S)
-    }
-    pval = 2 * pnorm(-abs(z))
-    pval0 = 2 * pnorm(-abs(z0))
-    Tau = S/(0.5 * n * (n - 1))
-    return(c(`Corrected Zc` = z, `new P-value` = pval, `N/N*` = essf, 
-        `Original Z` = z0, `old P.value` = pval0, Tau = Tau, 
-        `Sen's slope` = slp, old.variance = var.S, new.variance = VS))
-}
+
+
+# mmkh = function (x, ci = 0.95)
+#  {
+#     x = x
+#     z = NULL
+#     z0 = NULL
+#     pval = NULL
+#     pval0 = NULL
+#     S = 0
+#     Tau = NULL
+#     essf = NULL
+#     ci = ci
+#     if (is.vector(x) == FALSE) {
+#         stop("Input data must be a vector")
+#     }
+#     if (any(is.finite(x) == FALSE)) {
+#         x <- x[-c(which(is.finite(x) == FALSE))]
+#         warning("The input vector contains non-finite numbers. An attempt was made to remove them")
+#     }
+#     n <- length(x)
+#     V <- rep(NA, n * (n - 1)/2)
+#     k = 0
+#     for (i in 1:(n - 1)) {
+#         for (j in (i + 1):n) {
+#             k = k + 1
+#             V[k] = (x[j] - x[i])/(j - i)
+#         }
+#     }
+#     slp <- median(V, na.rm = TRUE)
+#     t = 1:length(x)
+#     xn <- (x[1:n]) - ((slp) * (t))
+#     for (i in 1:(n - 1)) {
+#         for (j in (i + 1):n) {
+#             S = S + sign(x[j] - x[i])
+#         }
+#     }
+#     ro <- acf(rank(xn), lag.max = (n - 1), plot = FALSE)$acf[-1]
+#     sig <- qnorm((1 + ci)/2)/sqrt(n)
+#     rof <- rep(NA, length(ro))
+#     for (i in 1:(length(ro))) {
+#         if (ro[i] > sig || ro[i] < -sig) {
+#             rof[i] <- ro[i]
+#         }
+#         else {
+#             rof[i] = 0
+#         }
+#     }
+#     cte <- 2/(n * (n - 1) * (n - 2))
+#     ess = 0
+#     for (i in 1:(n - 1)) {
+#         ess = ess + (n - i) * (n - i - 1) * (n - i - 2) * rof[i]
+#     }
+#     essf = 1 + ess * cte
+#     var.S = n * (n - 1) * (2 * n + 5) * (1/18)
+#     if (length(unique(x)) < n) {
+#         aux <- unique(x)
+#         for (i in 1:length(aux)) {
+#             tie <- length(which(x == aux[i]))
+#             if (tie > 1) {
+#                 var.S = var.S - tie * (tie - 1) * (2 * tie +
+#                   5) * (1/18)
+#             }
+#         }
+#     }
+#     VS = var.S * essf
+#     if (S == 0) {
+#         z = 0
+#         z0 = 0
+#     }
+#     if (S > 0) {
+#         z = (S - 1)/sqrt(VS)
+#         z0 = (S - 1)/sqrt(var.S)
+#     }
+#     else {
+#         z = (S + 1)/sqrt(VS)
+#         z0 = (S + 1)/sqrt(var.S)
+#     }
+#     pval = 2 * pnorm(-abs(z))
+#     pval0 = 2 * pnorm(-abs(z0))
+#     Tau = S/(0.5 * n * (n - 1))
+#     return(c(`Corrected Zc` = z, `new P-value` = pval, `N/N*` = essf,
+#         `Original Z` = z0, `old P.value` = pval0, Tau = Tau,
+#         `Sen's slope` = slp, old.variance = var.S, new.variance = VS))
+#  }
 
 qua_trend <- function(quantil=0.1, data_source = "q_long"){
   ken=matrix(nrow=catch_n, ncol=2)
