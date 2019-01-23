e3fb3d0e436029a508fa2519a3ece32ce1b56a28
bonopo <raphael.menke@posteo.de>
2019-01-23 18:18:32 +0100

    lm of ms30 ant cor

--- statistical.R
+++ statistical.R
@@ -50,3 +50,31 @@ which.min(mmky_su_sm_p$sen_slope)
 s= mmky_su_p_pet$S
 s_r = mean(s)
 var(s_r)
+#see barker ( i think)
+
+
+# linear regression -------------------------------------------------------
+
+#ms30 ~ long term memory effect
+
+
+
+
+data_plot = cbind.data.frame(y= mmky_ms30_min$sen_slope[mmky_ms30_min$new_p < 0.05],x1= gauges$lt_memoryeffect[mmky_ms30_min$new_p < 0.05], x2=gauges$mn_deficit[mmky_ms30_min$new_p < 0.05])
+
+fm= lm(mmky_ms30_min$sen_slope[mmky_ms30_min$new_p < 0.05] ~ gauges$lt_memoryeffect[mmky_ms30_min$new_p < 0.05]) %>% summary()
+
+hist(data_plot$x2)
+residuals(fm) %>% hist()
+
+ggplot(data= data_plot, aes(y=y, x=x1, col=x2))+
+  geom_point()+
+  geom_smooth(method="lm", se = TRUE, show.legend = F)+
+  annotate( geom="text", -Inf, -Inf,  hjust = -0.2, vjust = -2.5, label=paste("n = ", length(data_plot$y)))+
+  annotate(geom="text", -Inf, -Inf,  hjust = -0.2, vjust = -1, label=paste("p = 0.05"))+
+  annotate(geom="text", -Inf, -Inf,  hjust =-0.2, vjust = -4, label=paste("r²=",round(fm$adj.r.squared,2)))+
+  xlab(paste("lt memory effect [cor. ssi ~ spi-12]"))+
+  ylab(paste("ms30 trend (slope)"))+
+  scale_color_continuous("mean drought \n deficit ln [m³]")
+
+ggsave("./plots/statistical/ms30_ltmemory.png")
